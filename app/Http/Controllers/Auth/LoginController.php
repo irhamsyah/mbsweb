8c5c40a1ec28597ceba443414843ad1d1c1a58d8
BastianAndy <bbaztanzi@gmail.com>
2023-01-16 15:31:21 +0800

    perubahan route web; perubahan CS page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -34,7 +34,7 @@ class LoginController extends Controller
      * @return void
      */
     public function __construct()
-    {
+    {   
         $this->middleware('guest')->except('logout');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header text-center">{{ __('Sign in terlebih dahulu untuk memulai session') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">

e6dd45e6e1ea6a603fb5bc1f24e5cb95a209f9ca
irham <irhamp12@gmail.com>
2024-02-13 12:49:29 +0700

    app/Exports/ReportNominGrpJnsDepExport.php

---
+++ app/Exports/ReportNominGrpJnsDepExport.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Exports;
+
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\Exportable;
+
+class ReportNominGrpJnsDepExport implements FromView
+{
+    public $array;
+    use Exportable;
+
+    public function __construct(array $array)
+    {
+        $this->array = $array;
+    }
+
+    public function view(): View
+    {
+        return view('exports.deposito.nominatifgrpperjnsexcel',[
+            'nomin'=>$this->array
+        ]);
+    }
+}
